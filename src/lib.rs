8902c8a6a0d6d7723e35fb2a70eb4c791cb1ab1c
direct <github@itsdirect.dev>
2023-09-16 18:44:04 +0200

    Implement Boyer-Moore-Horspool for patterns

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,7 @@ impl<'a> Pattern {
         Matches {
             pattern: self,
             data,
+            skip_table: build_skip_table(self),
             index: 0,
         }
     }
@@ -63,6 +64,7 @@ impl PartialEq<[u8]> for Pattern {
 pub struct Matches<'a> {
     pattern: &'a Pattern,
     data: &'a [u8],
+    skip_table: Vec<usize>,
     index: usize,
 }
 
@@ -70,16 +72,38 @@ impl Iterator for Matches<'_> {
     type Item = usize;
 
     fn next(&mut self) -> Option<Self::Item> {
-        while *self.pattern != self.data[self.index..] {
-            if self.index == self.data.len() {
-                return None;
+        while self.index <= self.data.len() - self.pattern.0.len() {
+            if *self.pattern == self.data[self.index..] {
+                let index = self.index;
+                self.index += self.pattern.0.len();
+                return Some(index);
             }
 
-            self.index += 1;
+            let last_index = self.pattern.0.len() - 1;
+            self.index += self.skip_table[self.data[self.index + last_index] as usize];
         }
 
-        let index = self.index;
-        self.index += self.pattern.0.len();
-        Some(index)
+        None
     }
+}
+
+fn build_skip_table(pattern: &Pattern) -> Vec<usize> {
+    let last_index = pattern.0.len() - 1;
+    let mut max_skip = pattern.0.len();
+
+    for i in 0..last_index {
+        if let MaskedByte(None) = pattern.0[i] {
+            max_skip = last_index - i;
+        }
+    }
+
+    let mut skip_table = vec![max_skip; 256];
+
+    for i in 0..last_index {
+        if let MaskedByte(Some(byte)) = pattern.0[i] {
+            skip_table[byte as usize] = last_index - i;
+        }
+    }
+
+    skip_table
 }
