cfedc611834e335c40e0f21c0af28aa1ce24179c
direct <github@itsdirect.dev>
2025-05-23 21:19:38 +0200

    Simplify crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,110 @@
-mod horspool;
-mod memchr;
-mod naive_search;
-mod pattern;
+use std::num::ParseIntError;
+use std::str::FromStr;
 
-pub use horspool::*;
-pub use memchr::*;
-pub use naive_search::*;
-pub use pattern::*;
+pub struct MaskedByte(pub Option<u8>);
 
-pub trait Searcher<'a>: Iterator<Item = usize> {
-    fn new(pattern: &'a Pattern, data: &'a [u8]) -> Self;
+impl FromStr for MaskedByte {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s == "?" {
+            return Ok(Self(None));
+        }
+
+        let byte = u8::from_str_radix(s, 16)?;
+        Ok(Self(Some(byte)))
+    }
+}
+
+impl PartialEq<u8> for MaskedByte {
+    fn eq(&self, other: &u8) -> bool {
+        match self.0 {
+            Some(byte) => byte == *other,
+            None => true,
+        }
+    }
+}
+
+pub struct Pattern(pub Vec<MaskedByte>);
+
+impl<'a> Pattern {
+    pub fn matches(&'a self, data: &'a [u8]) -> impl Iterator<Item = usize> + use<'a> {
+        Matches::new(self, data)
+    }
+}
+
+impl FromStr for Pattern {
+    type Err = ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let bytes = s
+            .split_ascii_whitespace()
+            .map(str::parse)
+            .collect::<Result<Vec<_>, _>>()?;
+
+        Ok(Self(bytes))
+    }
+}
+
+impl PartialEq<[u8]> for Pattern {
+    fn eq(&self, other: &[u8]) -> bool {
+        if self.0.len() > other.len() {
+            return false;
+        }
+
+        self.0.iter().zip(other.iter()).rev().all(|(a, b)| a == b)
+    }
+}
+
+pub struct Matches<'a> {
+    pattern: &'a Pattern,
+    prefix: usize,
+    first: u8,
+    data: &'a [u8],
+    index: usize,
+}
+
+impl<'a> Matches<'a> {
+    fn new(pattern: &'a Pattern, data: &'a [u8]) -> Self {
+        let (prefix, first) = pattern
+            .0
+            .iter()
+            .enumerate()
+            .find_map(|(i, b)| b.0.map(|b| (i, b)))
+            .unwrap();
+
+        Self {
+            pattern,
+            prefix,
+            first,
+            data,
+            index: 0,
+        }
+    }
+}
+
+impl Iterator for Matches<'_> {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.index <= self.data.len() - self.pattern.0.len() {
+            let mut offset = memchr::memchr(self.first, &self.data[self.index..])?;
+
+            if offset >= self.prefix {
+                offset -= self.prefix;
+            }
+
+            self.index += offset;
+
+            if *self.pattern == self.data[self.index..] {
+                let index = self.index;
+                self.index += self.pattern.0.len();
+                return Some(index);
+            } else {
+                self.index += 1;
+            }
+        }
+
+        None
+    }
 }
